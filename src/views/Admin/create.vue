39f30d39997e31c9496c4c2a139392f1b43f6735
Muhamad Irhashdianto <dev.irhashdianto@gmail.com>
2022-03-25 03:19:11 +0700

    Feat: Validation form

--- src/views/Admin/create.vue
+++ src/views/Admin/create.vue
@@ -1,18 +1,20 @@
 <template>
     <mp-dialog title="Create Post">
         <el-form
+            ref="ruleFormRef"
             label-position="top"
             label-width="100px"
             :model="form"
+            :rules="rules"
         >
-            <el-form-item label="Title">
+            <el-form-item label="Title" prop="title">
                 <el-input v-model="form.title" />
             </el-form-item>
-            <el-form-item label="Pos">
+            <el-form-item label="Body" prop="body">
                 <el-input type="textarea" v-model="form.body" />
             </el-form-item>
             <el-space :size="15">
-                <el-button @click="submit" type="primary" size="large">
+                <el-button @click="submit(ruleFormRef)" type="primary" size="large">
                     Submit
                 </el-button>
                 <el-button type="default" size="large">
@@ -25,34 +27,59 @@
 
 <script>
 import MpDialog from '@/components/MpDialog/index.vue'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
     components: { MpDialog },
     setup () {
+        const ruleFormRef = ref(null)
         let form = reactive({
             userId: '',
             title: '',
             body: ''
         })
+        let rules = reactive({
+            title: [
+                {
+                    required: true,
+                    message: 'Field Is Required',
+                    trigger: 'blur'
+                }
+            ],
+            body: [
+                {
+                    required: true,
+                    message: 'Field Is Required',
+                    trigger: 'blur'
+                }
+            ]
+        })
         let store = useStore()
-        const submit = async () => {
-            form.userId = (JSON.parse(localStorage.userMyPost)).id
-            try {
-                await store.dispatch('global/createPost', form)
-                ElNotification({
-                    title: 'Success',
-                    message: `Success Creating Post`,
-                    type: 'success',
-                })
-            } catch (error) {
-                alert(error)
-            }
+
+        const submit = async (elmref) => {
+            if (!elmref) return
+            await elmref.validate(async (valid, fields) => {
+                if (valid) {
+                    form.userId = (JSON.parse(localStorage.userMyPost)).id
+                    try {
+                        await store.dispatch('global/createPost', form)
+                        ElNotification({
+                            title: 'Success',
+                            message: `Success Creating Post`,
+                            type: 'success',
+                        })
+                    } catch (error) {
+                        alert(error)
+                    }
+                }
+            })
         }
         return {
             form,
-            submit
+            submit,
+            ruleFormRef,
+            rules
         }
     }
 }
